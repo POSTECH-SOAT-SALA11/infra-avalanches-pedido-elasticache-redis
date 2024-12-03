305bf1e623db7bacd78f5241a8a556795219a1ac
raphasteodoro <154240784+raphasteodoro@users.noreply.github.com>
2024-12-02 21:30:49 -0300

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -82,3 +82,22 @@ output "redis_primary_endpoint" {
 output "redis_port" {
   value = aws_elasticache_replication_group.redis_replication_group.port
 }
+
+output "secrets_id" {
+  value = aws_secretsmanager_secret.db_credentials.id
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name                    = "pedido-dbcredentials"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials_version" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    redis_host     = aws_elasticache_replication_group.redis_replication_group.primary_endpoint_address
+    redis_port     = aws_elasticache_replication_group.redis_replication_group.port
+    redis_password = ""
+    typeorm        = "postgres://${random_string.username.result}:${random_string.password.result}@${aws_db_instance.postgresdb.address}:${aws_db_instance.postgresdb.port}/${aws_db_instance.postgresdb.db_name}"
+  })
+}
